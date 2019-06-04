c6db70bf0856ef9a1041bba2f98a147f25099e7b
ZiyadSultan <zsultan@wisc.edu>
2019-06-04 14:10:22 -0700

    committing some initial changes to runBatchROIAnalysis and rerunBinary

--- rerunBinary.m
+++ rerunBinary.m
@@ -1,10 +1,17 @@
-function [] = rerunBinary(date,exptIDs)
+function [] = rerunBinary(animal,date,exptIDs)
 
 for iExpt = 1:length(exptIDs)
     close all;
     exptname = [date '-' exptIDs{iExpt}];
+    index = exptIDs{iExpt};
     
-    prevAnalysis = load(['M:\PassiveEphys\2019\' exptname '\' exptname '-movementBinary.mat']);
+    prevAnalysis = load(['M:\PassiveEphys\20' date(1:2) '\' exptname '\' exptname '-movementBinary.mat']);
     fullROI = prevAnalysis.fullROI;
-    roiVidAnalysisBinary(['W:\Data\PassiveEphys\2019\' exptname '\2019_' exptname '_Cam1.avi'],exptname(1:5),exptname(7:9),false,fullROI,true,true);
+    
+    if str2double(animal(end-1:end)) > 51 %assumes animal name has a number associated with it (and follows the EEG animal order). New system videos started with EEG52
+        vidFileName = ['W:\Data\PassiveEphys\20' date(1:2) '\' date '-' index '\20' date(1:2) '_' date '-' index '_Cam1.avi'];
+    else
+        vidFileName = ['W:\Data\PassiveEphys\20' date(1:2) '\' date '-' index '\' date '-' index ];
+    end
+    roiVidAnalysisBinary(vidFileName,date,index,false,fullROI,true,true);
 end
--- runBatchROIAnalysis.m
+++ runBatchROIAnalysis.m
@@ -1,6 +1,9 @@
-function runBatchROIAnalysis(animal)
+function runBatchROIAnalysis(animal,useOldROI)
 % animal = 'EEG74';
 % animal = 'EEG68';
+if nargin < 2
+   useOldROI = false; % if there are no saved ROIs for this animal/date, set firstTime equal to true 6/3/2019 
+end
 
 query = 'Spon';
 listOfExpts = getExperimentsByAnimal(animal,query);
@@ -15,7 +18,6 @@ end
 dates = unique(a)';
 % dates = dates(end-2:end); %for EEG68 and 69... 
 
-
 for ii = 1:length(dates)
     date = dates{ii};
     list = getExperimentsByAnimalAndDate(animal,date);
@@ -25,12 +27,11 @@ for ii = 1:length(dates)
     end
     for jj = 1:length(indices)
         index = indices{jj};
-%         vidFileName = ['W:\Data\PassiveEphys\20' date(1:2) '\' date '-' index '\2019_' date '-' index '_Cam1.avi'];
-        vidFileName = ['W:\Data\PassiveEphys\20' date(1:2) '\' date '-' index '\' date '-' index ];
-        [~,fullROI] = roiVidAnalysisBinary(vidFileName,date,index,false,fullROI,true);
+        if str2double(animal(end-1:end)) > 51 % assumes animal name has a number associated with it (and follows the EEG animal order). New system videos started with EEG52
+            vidFileName = ['W:\Data\PassiveEphys\20' date(1:2) '\' date '-' index '\20' date(1:2) '_' date '-' index '_Cam1.avi'];
+        else
+            vidFileName = ['W:\Data\PassiveEphys\20' date(1:2) '\' date '-' index '\' date '-' index ];
+        end
+        [~,fullROI] = roiVidAnalysisBinary(vidFileName,date,index,fullROI,useOldROI);
     end
 end
-
-%         subplot(length(fileNameList),1,iFile)
-%         plot(finalMovementArray);
-%         title(fileName(end-8:end));
