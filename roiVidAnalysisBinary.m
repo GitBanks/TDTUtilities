771a6dc72783562232788d8ba83fdb4e75635603
ZiyadSultan <zsultan@wisc.edu>
2019-06-04 07:41:20 -0700

    roiVidAnalysisBinary: removed code related to Matt's mouse-luminance correction in drawMouseROI, along with older, unused code from previous versions videoROIBrainwareLED: **changed output time array from 'times' to 'newTimes'**, added an initial fix for if timeGrid and times are not equal, added comments throughout, added check for if ledFrame happens in first few elements of video

--- roiVidAnalysisBinary.m
+++ roiVidAnalysisBinary.m
@@ -1,4 +1,4 @@
-function [roiPix,fullROI] = roiVidAnalysisBinary(vidFileName,date,index,loadRoiPix,fullROI,skipMouse,useOldROI)
+function [roiPix,fullROI] = roiVidAnalysisBinary(vidFileName,date,index,fullROI,useOldROI)
 % GIVEN: a file location and (optional) existing 240x320 logical. 
 % DO: load video file; prompt user to draw ROI; read video 60s at a
 % time & exclude pixels outside ROI; calculate 1st derivative across all pixels, average, and
@@ -14,15 +14,10 @@ addpath('Z:\DataBanks\mmread');
 % example parameters:
 % vidFileName = 'W:\Data\PassiveEphys\2018\18410-000\18410-000'; %EEG51  %108905
 % vidFileName = 'W:\Data\PassiveEphys\2019\19404-000\2019_19404-000_Cam1.avi'; %EEG74 %36152
-% vidFileName = 'W:\Data\PassiveEphys\2017\17512-000\17512-000'; %EEG29 
-% vidFileName = 'W:\Data\PassiveEphys\2019\19310-001\2019_19310-001_Cam1.avi'; %EEGRoboMouse %603
-% vidFileName = 'W:\Data\PassiveEphys\2019\19425-000\2019_19425-000_Cam1.avi'; %EEG76
-% roiPix = [];
 
-if nargin<7
+if nargin<5
     useOldROI = false;
 end
-
 
 % make sure file or non .avi video version exist (copied from Sean's code)
 if exist(vidFileName) ~= 2 
@@ -62,72 +57,43 @@ close(qq);
 h = firstFrame.height;
 w = firstFrame.width;
 
-% draw ROI & use shape to create logical to exclude pixels as nans in subsequent steps
-if nargin<4
-    loadRoiPix = false;
-end
-if nargin<6
-    skipMouse = false;
-end
-
-if loadRoiPix
-    load(['M:\PassiveEphys\20' date(1:2) '\' date '-' index '\' date '-' index '-movement.mat'],'roiPix');
-end
-
-
-if ~exist('roiPix','var') || isempty(roiPix)
+if ~exist('roiPix','var')
     if exist('fullROI','var') && ~isempty(fullROI)
-        [roiPix,~,musArea,bkgdArea,musLum,bkgdLum,fullROI,mouseROI] = drawMouseROI(firstFrame,currentFrame,fullROI,skipMouse,useOldROI);
+        [roiPix,fullROI] = drawMouseROI(firstFrame,currentFrame,fullROI,useOldROI);
     else
-        [roiPix,~,musArea,bkgdArea,musLum,bkgdLum,fullROI,mouseROI] = drawMouseROI(firstFrame,currentFrame,[],true);
+        [roiPix,fullROI] = drawMouseROI(firstFrame,currentFrame,[],useOldROI);
     end
 else
-    figure('name',['time = ' num2str(firstFrame.times) 'sec'],'Position',[261 119 319 854]);
+    figure('name',['time = ' num2str(firstFrame.times) 'sec']);%,'Position',[261 119 319 854]);
     currentFrame(~roiPix) = nan;
     imshow(currentFrame);
 end
 
-
 tic
-% read whole video into wholeVid structure
 disp('reading video using mmread');
 wholeVid = mmread(vidFileName);
-
-
-%preallocate arrays
 
 disp('preallocating arrays');
 nFrames = length(wholeVid.frames);
 frames = zeros(h,w,nFrames,'uint8');
 
-
-%convert each frame to grayscale and set pixels outside ROI to zero. 
-
 disp('converting frames to grayscale & excluding pixels outside ROI');
 for iFrame = 1:nFrames
     frames(:,:,iFrame) = rgb2gray(wholeVid.frames(iFrame).cdata); % store in frames variable for this iteration
-   
 end
-times = wholeVid.times;
+times = wholeVid.times; % important! this is going to become the new time array
 
 
-
-
-% Is this synapse data or old data?
+% determine if this is synapse data or old data
 [synapseData,frameTimeStamps] = isSynapseData(vidFileName);
 
 if ~synapseData
-    [framesToKeep,times,~] = videoROIBrainwareLED(vidFileName,times);
-    
-    % KILL FRAMES
-    %times = times(framesToKeep);
-    %frames = frames(:,:,framesToKeep);
-    
+    [framesToKeep,times,~] = videoROIBrainwareLED(vidFileName,times);    
 else
     [framesToKeep,times] = synapseFrameTimeAdjust(times,frameTimeStamps);
 end
 
-
+% display Bryan's magic
 figure()
 ninetyfifth = prctile(frames(:,:,framesToKeep),95,3);
 imshow(ninetyfifth);
@@ -144,28 +110,6 @@ imshow(ptoneth);
 scale = double(ninetyfifth-ptoneth);
 scale(scale<50) = 0;
 
-%% DEBUG
-% figure()
-% tic
-% for i = 1:size(frames,3)
-% 
-%    subframe = double(frames(:,:,i)-ptoneth);
-% 
-%    scaleframe = true(size(subframe));
-%    scaleframe(subframe>scale*.3) = 0;
-% 
-%    imshow(scaleframe);
-%    pause(.01);
-% end
-
-% figure()
-% tic
-% for i = 1:size(frames,3)
-%    imagesc(squeeze(frames(:,:,i)));
-%    title(i);
-%    pause(.01);
-% end
-% toc
 %%
 
 %figure()
@@ -202,7 +146,6 @@ times = times(framesToKeep(1:end-1));
 % 2D smoothing (this takes >100 seconds
 
 disp('2D smoothing')
-%sm_temp_dFrames = nan(h,w,nFrames-1);
 sm_temp_dFrames_avg = nan(size(temp_dFrames,3),1);
 parfor i = 1:size(temp_dFrames,3)
     sm_temp = imfilter(temp_dFrames(:,:,i), ones(7)/7^2); % 2D smoothing filter - are we satisfied with this?
@@ -211,10 +154,8 @@ parfor i = 1:size(temp_dFrames,3)
     %pause(.01);
     
     sm_temp(~roiPix) = nan; % exclude nans
-    %sm_temp_dFrames_avg(i) = nanmean(sm_temp(roiPix));
     sm_temp_dFrames_avg(i) = nanmean(sm_temp,'all');
     
-
 end
 
 
@@ -224,19 +165,10 @@ end
 %    pause(.01);
 % end
 
-
 % smooth the difference array across time and subtract minimum value to
 % correct floor differences
 avg_FR = size(frames,3)/max(times);
-% finalMovementArray = smooth(sm_temp_dFrames_avg);
 finalMovementArray = smooth(sm_temp_dFrames_avg-min(sm_temp_dFrames_avg),2*floor(avg_FR)+1);
-
-% perform adjustment based on size and luminance of mouse/background 
-%finalMovementArray = finalMovementArray/((musArea/bkgdArea)*(bkgdLum-musLum)); 
-
-% calculate average frame rate & divid movement signal by that
-%avg_FR = size(frames,3)/max(times);
-%finalMovementArray = finalMovementArray*avg_FR; %multiply by average frame rate
 
 toc;
 
@@ -247,21 +179,18 @@ if ~exist(['M:\PassiveEphys\20' date(1:2) '\' date '-' index],'dir')
    mkdir(['M:\PassiveEphys\20' date(1:2) '\' date '-' index]);
 end
 
-save(['M:\PassiveEphys\20' date(1:2) '\' date '-' index '\' date '-' index '-movementBinary.mat'],'finalMovementArray','frameTimeStampsAdj','roiPix','fullROI','mouseROI','avg_FR');
+save(['M:\PassiveEphys\20' date(1:2) '\' date '-' index '\' date '-' index '-movementBinary.mat'],'finalMovementArray','frameTimeStampsAdj','roiPix','fullROI','avg_FR');
 
 end
 
-function [roiPix,musPix,musArea,bkgdArea,musLum,bkgdLum,fullROI,mouseROI] = drawMouseROI(firstFrame,currentFrame,fullROI,skipMouse,useOldROI)
+function [roiPix,fullROI] = drawMouseROI(firstFrame,currentFrame,fullROI,useOldROI)
 %it's in the title. Use with final product of videoROI analysis
-figure('name',['time = ' num2str(firstFrame.times) 'sec'],'Position',[520,100,461,700]);
-% subtightplot(1,2,1);
+figure('name',['time = ' num2str(firstFrame.times) 'sec']);%,'Position',[520,100,461,700]);
 disp('INPUT REQUIRED: draw a closed shape around bottom of cage');
-%[roiPix] = roipoly(currentFrame); % background + mouse ROI selection
 
-if nargin<5
+if nargin<4
     useOldROI = false;
 end
-
 
 h = imshow(currentFrame);
 if ~exist('fullROI','var') || isempty(fullROI)
@@ -269,9 +198,6 @@ if ~exist('fullROI','var') || isempty(fullROI)
 else
     disp('using existing ROI')
     
-    %fullROI.Image = h;
-    %fullROI.Parent = gca;
-    %fullROI.Selected = true;
     [newfullROI] = drawassisted(h,'Position',fullROI.Position,'Waypoints',fullROI.Waypoints);
     if ~useOldROI
         customWait(newfullROI);
@@ -285,41 +211,12 @@ else
     end
 end
 
-
-if nargin<4
-    skipMouse = false;
-end
-
 roiPix = createMask(fullROI);
 disp('background ROI selected!');
 bkgd = currentFrame;
 bkgd(~roiPix) = nan;
-% subtightplot(1,2,2);
-% disp('step 2) draw a closed shape around animal');
+
 figure
-h2 = imshow(bkgd);
-mus = bkgd; % create mouse image variable before drawing ROI
-%[musPix] = roipoly(bkgd); % mouse ROI selection
-if skipMouse
-    musPix=[];
-    musArea=1;
-    bkgdArea=1;
-    musLum=0;
-    bkgdLum=1;
-    mouseROI=[];
-else
-    [mouseROI] = drawassisted(h2);
-    
-    musPix = createMask(mouseROI);
-    disp('mouse ROI selected!');
-    subtightplot(3,1,3);
-    mus(~musPix) = nan; % exclude all non-mouse pixels
-    bkgd(musPix) = nan; % exclude mouse pixels from background
-    musArea = sum(musPix,'all'); % area of mouse (number of pixels)
-    bkgdArea = sum(roiPix-musPix,'all'); % area of background (number of pixels)
-    musLum = nanmean(mus,'all'); % mean luminance of mouse
-    bkgdLum = nanmean(bkgd,'all'); % mean luminance of background (no mouse)
-end
 imshow(bkgd);
 
 end
--- videoROIBrainwareLED.m
+++ videoROIBrainwareLED.m
@@ -1,4 +1,4 @@
-function [keepFrames,times,finalLEDTimes] = videoROIBrainwareLED(vidFileName,times)
+function [keepFrames,newTimes,finalLEDTimes] = videoROIBrainwareLED(vidFileName,times)
 
 
 % procedure for aligning existing LED pulse information with corresponding
@@ -19,13 +19,13 @@ rawPath = vidFileName(1:delims(end));
 
 % 1. load existing LED pulse information
 try
-    load([rawPath vidFileName(delims(end)+1:end) '-movementInfo.mat'],'finalLEDTimes','timeGrid');
+    load([rawPath vidFileName(delims(end)+1:end) '-movementInfo.mat'],'finalLEDTimes','timeGrid','nMissingTrials','frameGridLEDElement');
 catch
     warning('movementInfo file not found in W');
 end
 if ~exist('finalLEDTimes','var')
     try
-        load(['\\memorybanks' rawPath(delims(1):delims(end)) vidFileName(delims(end)+1:end) '-movementInfo.mat'],'finalLEDTimes','timeGrid');
+        load(['\\memorybanks' rawPath(delims(1):delims(end)) vidFileName(delims(end)+1:end) '-movementInfo.mat'],'finalLEDTimes','timeGrid','nMissingTrials','frameGridLEDElement');
     catch
         error('movementInfo file not found in memorybanks');
     end
@@ -34,52 +34,79 @@ if ~exist('finalLEDTimes','var')
     error('movementInfo file not found');
 end
 
-% 2. load ephys
+% 2. load ephys trial info
 ephysDirName = ['\\memorybanks' rawPath(delims(1):delims(end))];
 tempEphysTrialTime = loadTrialList(ephysDirName);
 
-
-if ~isequal(timeGrid,times)
-    error('timeGridNew not equal to timeGrid (old)');
+% check if timeGrid (saved) and times (new) are the same length. If they're not equal length,
+% then we need to see which times and trials we actually have and adjust accordingly. 
+if ~isequal(timeGrid,times) 
+    warning('timeGridNew not equal to timeGrid (old) ?_?');
+    figure; plot(frameGridLEDElement); % plot frameGridLEDElement, which is the saved LED flash array 
+    
+    % ask user to verify which section the frames are missing frameGridLEDElement
+    opts.Interpreter = 'tex'; % Use the TeX interpreter to format the question
+    opts.Default = 'start'; % default answer
+    quest = 'Based on the current figure, which segment of the video has no associated trials (LED pulses)?';
+    answer = questdlg(quest,'INPUT REQUIRED','start','end',opts);
+    
+    if strcmp(answer,'start')
+        
+        finalLEDTimes(1:nMissingTrials) = [];    % remove padded trials from video trial array
+        finalLEDTimes = finalLEDTimes - find(frameGridLEDElement>0,1); % subtract missing number of elements from the formerly padded array
+        tempEphysTrialTime(1:nMissingTrials) = [];  % remove padded trials from ephys trial array   
+    
+    elseif strcmp(answer,'end')
+        
+        finalLEDTimes(nMissingTrials:end) = [];
+        error(['haven' 't dealt with this case yet ¯\_(?)_/¯']); %not sure what to do in this case...
+        
+    end
 end
 
-clear timeGrid % delete loaded timeGrid and finalMovementArray.
+clear timeGrid % delete timeGrid (we don't need it)
 
-finalLEDFrames = finalLEDTimes;
+finalLEDFrames = finalLEDTimes; % rename, since finalLEDTimes isn't actually a time array, but contains the timeGrid elements with associated LED pulses
 
-% 3. determine ephys trial start times
-tempEphysTrialTime = tempEphysTrialTime(:) - tempEphysTrialTime(1); % relative to beginning of ephys start time
-tempLEDTrialTime = times(finalLEDFrames)';
+% 3. determine ephys trial start times relative to start
+tempEphysTrialTime = tempEphysTrialTime(:) - tempEphysTrialTime(1); % relative to beginning of ephys trials
+
+
+tempLEDTrialTime = times(finalLEDFrames)'; 
 if length(tempEphysTrialTime) ~= length(tempLEDTrialTime)
-   tempEphysTrialTime = tempEphysTrialTime(1:length(tempLEDTrialTime)); %Occurs when end of video is cut off
+   tempEphysTrialTime = tempEphysTrialTime(1:length(tempLEDTrialTime)); % occurs when end of video is cut off
+   warning('length(tempEphysTrialTime) ~= length(tempLEDTrialTime)');
 end
 
-
-
-newTimes = times - times(finalLEDFrames(1));
+newTimes = times - times(finalLEDFrames(1)); % times is now relative to the first LED pulse and cast as a different variable
 
 for iLED = 2:length(finalLEDFrames)
-    % Adjust timeGridnew
-    start = finalLEDFrames(iLED-1);
-    stop = finalLEDFrames(iLED);
-    startTime = tempEphysTrialTime(iLED-1);
-    stopTime = tempEphysTrialTime(iLED);
     
-    startVidTime = tempLEDTrialTime(iLED-1);
-    stopVidTime = tempLEDTrialTime(iLED);
+    start = finalLEDFrames(iLED-1); % interval start element
+    stop = finalLEDFrames(iLED); % interval end element
     
-    scaleFactor = (stopTime-startTime)/(stopVidTime-startVidTime);
+    startTime = tempEphysTrialTime(iLED-1); % ephys trial start time
+    stopTime = tempEphysTrialTime(iLED); % ephys trial end time
     
-    newTimes(start:stop) = (times(start:stop)-times(start))*scaleFactor + newTimes(start);
+    startVidTime = tempLEDTrialTime(iLED-1); % video trial start time
+    stopVidTime = tempLEDTrialTime(iLED); % video trial end time
+    
+    %compare ephys trial time (assumed to be accurate) 
+    %to video trial time (assumed to be complete BS) and calculate a scale factor to adjust 
+    scaleFactor = (stopTime-startTime)/(stopVidTime-startVidTime); 
+    
+    newTimes(start:stop) = (times(start:stop)-times(start))*scaleFactor + newTimes(start); % Bryan magic
 end
 
-
+% Exclude frames near LED times, just to be cautious
 keepFrames = true(size(times));
 for iLED = 1:length(finalLEDFrames)
-    % Exclude frames near LED times
     ledFrame = finalLEDFrames(iLED);
-    
-    keepFrames((ledFrame-2):ledFrame) = false;
+    if ledFrame-2 < 1 %if the LED occurs too close to the start, we can't subtract two frames before...
+        keepFrames(1:ledFrame) = false; %ADDED 5/31/2019
+    else
+        keepFrames((ledFrame-2):ledFrame) = false;
+    end
     discardInterval = find(newTimes>(newTimes(ledFrame)+.7),1,'first');
     keepFrames(ledFrame:discardInterval) = false;
 end
