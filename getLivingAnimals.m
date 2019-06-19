1793d9657508df2ca5d785825fc94725d57e2646
ZiyadSultan <zsultan@wisc.edu>
2019-06-18 19:49:18 -0500

    table2cell for getLivingAnimals created importDataSynapse_dual, synapseFrontEnd_dual for dual recordings!

--- getLivingAnimals.m
+++ getLivingAnimals.m
@@ -2,6 +2,7 @@ function [livingAnimals,livingAnimalsID] = getLivingAnimals()
 
 dbConn = dbConnect();
 livingAnimals = fetch(dbConn,'select animalName from animals where sacDate=''0000-00-00''');
+livingAnimals = table2cell(livingAnimals);
 if isempty(livingAnimals)
     error('Animal name not found! Check spelling.')
 end
