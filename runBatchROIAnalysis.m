a3d56beaed10ed8adce91d9c3f4228cb0badbe26
ZiyadSultan <zsultan@wisc.edu>
2019-06-21 18:42:28 -0500

    fix in isSynapseData for when the index is not the blockIndex (related to dual recording) fix in runBatchROIAnalsis for checking video file (fix for dual recording) hotfix in videoROIBrainwareLED for when video file name contains the 2019 string...

--- runBatchROIAnalysis.m
+++ runBatchROIAnalysis.m
@@ -19,16 +19,23 @@ for ii = 1:length(dates)
     date = dates{ii};
     list = getExperimentsByAnimalAndDate(animal,date);
     fullROI = [];
-    for qq = 1:length(list)
+    for qq = 1:size(list,1)
         indices{qq} = list{qq,1}(7:9);
     end
     for jj = 1:length(indices)
         index = indices{jj};
         if str2double(animal(end-1:end)) > 51 % assumes animal name has a number associated with it (and follows the EEG animal order). New system videos started with EEG52
-            vidFileName = ['W:\Data\PassiveEphys\20' date(1:2) '\' date '-' index '\20' date(1:2) '_' date '-' index '_Cam1.avi'];
+            vidFileName = ['W:\Data\PassiveEphys\20' date(1:2) '\' date '-' index '\20' date(1:2) '_' date '-' index '_Cam*.avi'];
         else
-            vidFileName = ['W:\Data\PassiveEphys\20' date(1:2) '\' date '-' index '\' date '-' index ];
+%             vidFileName = ['W:\Data\PassiveEphys\20' date(1:2) '\' date '-' index '\' date '-' index ];
+            vidFileName = ['W:\Data\PassiveEphys\20' date(1:2) '\' date '-' index '\' date '-' index '-converted.mp4']; %ONLY FOR EEG43 and 47
         end
-        [~,fullROI] = roiVidAnalysisBinary(vidFileName,date,index,fullROI,false);
+        dirCheck = dir(vidFileName);
+        if ~isempty(dirCheck)
+            vidFileName = [dirCheck.folder '\' dirCheck.name];
+            [~,fullROI] = roiVidAnalysisBinary(vidFileName,date,index,fullROI,false);
+        else 
+            disp([vidFileName ' not found']);
+        end
     end
 end
