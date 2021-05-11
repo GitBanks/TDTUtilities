b09ec4ff1d4768361938e625d9a4c1f41a0f1524
ZiyadSultan <zsultan@wisc.edu>
2021-05-11 14:36:08 -0500

    weird change from a million years ago.  I don't even remember using this function.   Revert if you care about this.

--- plotAllBehaveAnimalsInDay.m
+++ plotAllBehaveAnimalsInDay.m
@@ -1,17 +1,19 @@
-function plotAllBehaveAnimalsInDay(exptDate,sheet,treatmentTwo)
+function plotAllBehaveAnimalsInDay(exptDate,day,sheet,treatmentTwo)
 
-
+% example parameters:
 % exptDate = '19423';
+% day = 1; %e.g. for first day of experiment
 % treatmentTwo = 'PBS';
-% sheet = 'piroxicam'; %(name of the drug treatment, basically)
+% sheet = 'piroxicam'; %(name of the drug treatment)
+
+
 T = readtable('M:\mouseEEG\movement\behaviorExptDrugList.xlsx','Sheet',sheet);
 animals = T.animal;
 drugs = T.drug;
 
-
-for i=1:length(animals)
-    disp(['Loading ' animals{i}]);
-    [fullDayMovement(i,:),fullDayTimestamps(i,:)] = loadFullDayBehaveMovement(animals{i},exptDate);
+for ii=1:length(animals)
+    disp(['Loading ' animals{ii}]);
+    [fullDayMovement(ii,:),fullDayTimestamps(ii,:)] = loadFullDayBehaveMovement(animals{ii},exptDate);
 end
 
 disp('Finished loading.');
@@ -20,34 +22,45 @@ fullDayTimestamps = fullDayTimestamps(1,:); % careful! this assumes they're all 
 fullDayTimestamps = fullDayTimestamps/60/60;
 
 figureName = ['Daily Activity - ' exptDate ' - ' sheet ' - '  treatmentTwo];
-figure('Name',figureName,'Position',[60 80 1511 868]);
-for i=1:length(animals)
-    subtightplot(length(animals),1,i);
-    plot(fullDayTimestamps,fullDayMovement(i,:));
-    ylabel(animals{i});
-    %yticklabels([]);
+superTitle = ['Animal Movement - Day ' num2str(day) ' - ' treatmentTwo];
+figure('Name',figureName,'Position',[1 41 1600 1083]);
+
+for ii=1:length(animals)
+    h(ii) = subtightplot(length(animals),1,ii,[],[.075 .05],[.075 .05]);
+    plot(fullDayTimestamps,fullDayMovement(ii,:),'MarkerEdge',[.5 .5 .5]);
     ylim([0,yMax]);
-    vline(fullDayTimestamps(find(fullDayTimestamps>1,1)),'r',drugs{i});
-    vline(fullDayTimestamps(find(fullDayTimestamps>2,1)),'r',treatmentTwo);
+    l = vline(fullDayTimestamps(find(fullDayTimestamps>1,1)),'r',drugs{ii});
+    m = vline(fullDayTimestamps(find(fullDayTimestamps>2,1)),'r',treatmentTwo);
+    set(m,'LineWidth',2);
+    set(l,'LineWidth',2);
     xlim([0,fullDayTimestamps(end)])
-    xticks([0,0.5,1,1.5,2,2.5,3,3.5,4,4.5,5,5.5]);
     box off
-    xticklabels([]);
-    if i == length(animals)
-        xticklabels({'','hour 1','','hour 2','','hour 3','','hour 4','','hour 5','','hour 6'});
+    ax = gca;
+    ax.FontSize = 16; 
+    ax.LineWidth = 2;
+    if ii == length(animals)
+    else
+       yticklabels([]);
+       xticklabels([]);
     end
-    if i == 1
-        title(figureName);
+    if ii == 1
+        title(superTitle,'fontsize',20);
     end
+    if ii==length(animals)
+       xlabel('Time (hr)','fontsize',20); 
+    elseif ii==length(animals)-4
+       ylabel('\Delta luminance of video (a.u.)','fontsize',20); 
+    end
+
 end
 
+% v = findobj('String','minocycline');
+% w = findobj('String','PBS');
+% r = findobj('String','LPS');
+% set(v,'FontSize',14);
+% set(w,'FontSize',14);
+% set(r,'FontSize',14);
+
+linkaxes(h(:),'xy');
 outPath = 'M:\mouseEEG\movement\behavioral experiments\';
 savePlot(outPath,figureName);
-
-
-% treatmentOne = {'L-655,708';'PBS+DMSO';'L-655,708';'PBS+DMSO';'PBS+DMSO';'L-655,708';'PBS+DMSO';'L-655,708'}; %{'PBS','PBS','PBS','PBS','mino','mino','mino','mino'};
-% treatmentTwo = {'PBS','PBS','PBS','PBS','PBS','PBS','PBS','PBS'};
-% treatmentTwo = {'LPS','LPS','LPS','LPS','LPS','LPS','LPS','LPS'};
-
-% animals = {'MC04';'MC07';'MC16';'MC18';'MC20';'MC21';'MC22';'MC23'}; %{'MC04';'MC07';'MC16';'MC18';'MC20';'MC21';'MC22';'MC23'}; %{'MC09','MC11','MC12','MC15','MC08','MC10','MC13','MC14'};
-
