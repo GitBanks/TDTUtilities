40108dbeb7b2f53c5662c4d309f0f4e825bb9213
Sean Grady <smgrady@gmail.com>
2021-05-19 17:22:10 -0500

    fixed a small bug in checkConnection; standalone script made for Ephys analysis

--- checkConnection.m
+++ checkConnection.m
@@ -1,6 +1,6 @@
 function checkConnection(location)
     if exist(location,'dir') == 7
-        disp([thisIP ' connecting to ' location ' confirmed']);
+        disp([getMyIPAddress ' connecting to ' location ' confirmed']);
     else
         error(['Cannot connect ' thisIP ' to ' location ' Check connection to remote computer']);
     end
