c394873da38bd0e8b445ad10fbd88f039a0c8d26
Sean Grady <smgrady@gmail.com>
2021-09-29 09:47:11 -0500

    Added multi-day eCDF of movement to the UI, and the standalone function it runs.  Didn't push an earlier version, so this is another that takes a range of days and puts them on the same plot.

---
+++ plotMovementCDFAcrossDays.m
@@ -0,0 +1,33 @@
+function plotMovementCDFAcrossDays(animalName,subset)
+
+%subset is chosen in the UI - set of expts
+% example data:
+% animalName = 'ZZ10';
+% subset = {'21624';'21626';'21630'};
+
+
+useCDF = true;
+figure();
+for iExpt = 1:size(subset,1)
+%     subplot(1,size(subset,1),iExpt);
+    workingList = getExperimentsByAnimalAndDate(animalName,subset{iExpt},'spon');
+    workingList = workingList(1,:);
+    for iIndex = 1:size(workingList,1)
+        h = getMovementDataFromHTRByDateIndex(workingList{iIndex,1}(1:5),workingList{iIndex,1}(7:9),useCDF);
+        hold on
+        drawnow;
+    end
+end
+
+legend(subset,'Location','SouthEast');
+workingList = getExperimentsByAnimalAndDate(animalName,subset{1},'spon'); % just grab the first expt from list
+workingList = workingList(1,:);
+title(['Movement Across Days: ' workingList{1,2}{1,1}(1:10)]);
+xlim([0,2]);
+
+% for iExpt = 1:size(subset,1)
+%     subplot(1,size(subset,1),iExpt);
+%     title(workingList{iIndex,1}(1:5));
+% end
+
+
