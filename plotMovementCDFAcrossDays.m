373b56a7e38dfcbff2301f9c4e86637ee32d75af
Sean Grady <smgrady@gmail.com>
2021-10-11 11:13:02 -0500

    added a way to change which text we search for in the expt description

--- plotMovementCDFAcrossDays.m
+++ plotMovementCDFAcrossDays.m
@@ -1,16 +1,21 @@
-function plotMovementCDFAcrossDays(animalName,subset)
+function plotMovementCDFAcrossDays(animalName,subset,filterText)
 
 %subset is chosen in the UI - set of expts
 % example data:
 % animalName = 'ZZ10';
 % subset = {'21624';'21626';'21630'};
+% filterText = {'spon'}
+%filterText = 'stim/resp';
 
+if ~exist('filterText','var')
+    filterText = 'spon';
+end
 
 useCDF = true;
 figure();
 for iExpt = 1:size(subset,1)
 %     subplot(1,size(subset,1),iExpt);
-    workingList = getExperimentsByAnimalAndDate(animalName,subset{iExpt},'spon');
+    workingList = getExperimentsByAnimalAndDate(animalName,subset{iExpt},filterText);
     workingList = workingList(1,:);
     for iIndex = 1:size(workingList,1)
         h = getMovementDataFromHTRByDateIndex(workingList{iIndex,1}(1:5),workingList{iIndex,1}(7:9),useCDF);
@@ -20,7 +25,7 @@ for iExpt = 1:size(subset,1)
 end
 
 legend(subset,'Location','SouthEast');
-workingList = getExperimentsByAnimalAndDate(animalName,subset{1},'spon'); % just grab the first expt from list
+workingList = getExperimentsByAnimalAndDate(animalName,subset{1},filterText); % just grab the first expt from list
 workingList = workingList(1,:);
 title(['Movement Across Days: ' workingList{1,2}{1,1}(1:10)]);
 xlim([0,2]);
