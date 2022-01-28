aad708f5898f5b21d3b7bf0768a22a2b1b6f076e
smgrady <smgrady@gmail.com>
2022-01-27 18:55:20 -0600

    Aesthetic changes to plots

--- plotPlasticityDuringQuiescence.m
+++ plotPlasticityDuringQuiescence.m
@@ -1,11 +1,11 @@
 function plotPlasticityDuringQuiescence(animal,subset)
 
 
-
 %Run this from ephysAnalysisScript so we can get the peakData save file
 %this is the user selction of the peaks - make sure to clear all after
-exptDate = '21d31';
-exptIndices = {'002','004'};
+%Run this to decide the peaks and the peak amps
+exptDate = '22111';
+exptIndices = {'002','005','007'};
 description = '';
 plotPlasticityAmplitudePeaks(exptDate,exptIndices,description)
 
@@ -14,7 +14,7 @@ plotPlasticityAmplitudePeaks(exptDate,exptIndices,description)
 animal = 'ZZ14'; 
 
 %Define the subset of experiment days to use
-subset = {'21d31'}; 
+subset = {'22104'}; 
 
 %Create a table out of the experiment list which is called from exptPlasticitySet
 [~,exptTable] = getExptPlasticitySetByAnimal(animal);
@@ -28,7 +28,7 @@ exptSubTable = exptSubTable(exptSubTable.preLTP == true | exptSubTable.postLTP =
 %plotting portion- you will need to write out ALL the indices regardless of
 %whether you will plot them or not bc this is how we determine size of nIndex
 
-indexStepDescription = {'pre LTP','post LTP'};
+indexStepDescription = {'Baseline','post LTP'};
 
 
 %quick grab unique dates- we have to do this because there are multiple
@@ -73,7 +73,27 @@ ourIndex = 1;
     for iExptIndex = 1:nIndex
         disp("loop")
         ecdf(dataOut.date(iExptDate).expt(iExptIndex).data.restingPeaksIPSI(1,:));
-        if iExptDate == 1; title(['eCDF of IPSI Peak Evoked Response During Quiesensce ZZ14 21d30']); end
+        if iExptDate == 1; title(['eCDF of IPSI Peak Evoked Response During Quiesensce' animal subset ]); end
+        hold on;
+        if iExptDate == nExpts
+            ylabel('Probability');
+            xlabel('Amplitude of Response');
+            legend(indexStepDescription,'Location','southeast'); 
+        end
+           drawnow;
+           ourIndex = ourIndex+1;
+    end
+  end
+  
+% 2) Plotting Peak vCA1 evoked Response
+figure();
+subplot(2,3,[1,3]);
+ourIndex = 1;
+  for iExptDate = 1:nExpts
+    for iExptIndex = 1:nIndex
+        disp("loop")
+        ecdf(dataOut.date(iExptDate).expt(iExptIndex).data.restingPeaksVCA1(1,:));
+        if iExptDate == 1; title(['eCDF of IPSI Peak Evoked Response During Quiesensce' animal subset ]); end
         hold on;
         if iExptDate == nExpts
             ylabel('Probability');
@@ -100,7 +120,7 @@ subplot(2,3,4);
         %x = dataOut.date(iExptDate).expt(iExptIndex).data.restingValues(1,:)
         histogram(x, 'BinEdges', edges);
         grid on;
-        title(['Movement Distribution During Baseline']);
+        title(['Movement Distribution During ' indexStepDescription{1}]);
         ylabel('Frequency');
         xlabel('Amplitude of Movement');
         xticks(0:.2:1.5);
@@ -120,7 +140,7 @@ subplot(2,3,5);
         %x = dataOut.date(iExptDate).expt(iExptIndex).data.restingValues(1,:), 'FaceColor', '#D95319'
         histogram(x, 'BinEdges', edges, 'FaceColor', '#D95319');
         grid on;
-        title(['Movement Distribution Post-LTP']);
+        title(['Movement Distribution ' indexStepDescription{2}]);
         ylabel('Frequency');
         xlabel('Amplitude of Movement');
         xticks(0:.2:1.5);
@@ -141,7 +161,7 @@ subplot(2,3,6);
         %x = dataOut.date(iExptDate).expt(iExptIndex).data.restingValues(1,:)
         histogram(x, 'BinEdges', edges,'FaceColor', '#EDB120');
         grid on;
-        title(['Movement Distribution 2 hr Post-LTP']);
+        title(['Movement Distribution 'indexStepDescription{3}]);
         ylabel('Frequency');
         xlabel('Amplitude of Movement');
         xticks(0:.2:1.5);
@@ -194,82 +214,7 @@ subplot(2,3,6);
 %     end
 %   end
 
-  
-  %%%======================================================================
- %Determine what thresholds we should be using
-  
- 
-% 1) Pull in movement values
-nExpts = size(uniqueDates,1);
-nIndex = size(indexStepDescription,2);
-ourIndex = 1;
-for iExptDate = 1:nExpts 
-    for iExptIndex = 1:nIndex
-        thisDate = char(exptSubTable.DateIndex(ourIndex));
-        thisIndex = thisDate(7:9);
-        thisDate = thisDate(1:5);
-        [dataOut.date(iExptDate).expt(iExptIndex).data] = getPeakSlopeAvgByDateIndexWPlot(thisDate,thisIndex);
-        ourIndex = ourIndex+1;
-   end
-end
-% 2) Threshold values
-restingThresh = 0.3;
-restingValues = moveValuesForEachWindow(moveValuesForEachWindow<restingThresh);
 
-for iExptDate = 1:nExpts
-    for iExptIndex = 1
-baselineMeanMove = mean(dataOut.date(iExptDate).expt(iExptIndex).data.restingValues(1,:));
-    end 
-end
-
-
-
-syms x
-eqn = (mean(moveValuesForEachWindow > x) == baselineMeanMove);
-ANS = sollve
-
-
-%%% ==================================================
-doMovePlot = false;
-% pull needed data
-
-thisDate = iExptDate;
-thisIndex = '004';
-[~,moveValuesForEachWindow,peakValsForEachWindow] = plotStimAndMovement(thisDate,thisIndex);
-
-% the following were determined in getMovementDataFromHTRByDateIndex(exptDate,exptIndex,useCDF) so I'm not sure if we need to run it, or display it or what 
-restingThresh = 0.3;
-activeThresh = 0.15;
-theseAreResting = moveValuesForEachWindow<restingThresh;
-theseAreActive = moveValuesForEachWindow>activeThresh;
-
-restingValues = moveValuesForEachWindow(moveValuesForEachWindow<restingThresh);
-activeValues = moveValuesForEachWindow(moveValuesForEachWindow>activeThresh);
-
-
-% We need to pull the unaveraged trials again (because obviously, we're
-% sorting them)
-tPreStim = 0.02;
-tPostStim = 0.2;
-disp(['loading peaks for ' exptDate '-' exptIndex]);
-ephysData = getImportedSynapseEvokedData(exptDate,exptIndex,tPreStim,tPostStim);
-restingAvgMovement = mean(moveValuesForEachWindow(theseAreResting));
-curatedPeakValsForEachWindow = peakValsForEachWindow;
-
-
-%These are peak responses by ROI during just the quiet period
-for iROI = 1
-    restingPeaksIPSI = curatedPeakValsForEachWindow(iROI,theseAreResting),'omitnan';
-end
-
-for iROI = 2
-    restingPeaksCONTRA = curatedPeakValsForEachWindow(iROI,theseAreResting),'omitnan';
-end
-
-for iROI = 3
-    restingPeaksVCA1 = curatedPeakValsForEachWindow(iROI,theseAreResting),'omitnan';
-end
-  
   
  
   end
