a7c9a1b327302feca5d78aa36563efc2d4474937
Sean Grady <smgrady@gmail.com>
2022-04-05 11:11:27 -0500

    Finally plot eCDF across a whole day to show what happens each hour.

--- plotMovementCDFAcrossDays.m
+++ plotMovementCDFAcrossDays.m
@@ -5,34 +5,38 @@ function plotMovementCDFAcrossDays(animalName,subset,filterText)
 % animalName = 'ZZ10';
 % subset = {'21624';'21626';'21630'};
 % filterText = {'spon'}
+% 
+% animalName = 'Mag024';
+% subset = {'21d07'};
+% filterText = {'Spon'}
+
+
 %filterText = 'stim/resp';
 
 if ~exist('filterText','var')
-    filterText = 'spon';
+    filterText = 'Spon';
 end
 
 useCDF = true;
 figure();
 for iExpt = 1:size(subset,1)
-%     subplot(1,size(subset,1),iExpt);
+    subplot(1,size(subset,1),iExpt);
     workingList = getExperimentsByAnimalAndDate(animalName,subset{iExpt},filterText);
-    workingList = workingList(1,:);
+    %workingList = workingList(1,:);
     for iIndex = 1:size(workingList,1)
         h = getMovementDataFromHTRByDateIndex(workingList{iIndex,1}(1:5),workingList{iIndex,1}(7:9),useCDF);
         hold on
         drawnow;
+        exptDescription = workingList{iIndex,2}{1,1};
+        [~,hourText,~] = getParsedExperimentDescriptionFromText(exptDescription);
+        listBuilder{iIndex} = hourText;
     end
+
+    [exptTitle,~,~] = getParsedExperimentDescriptionFromText(exptDescription);
+    title([animalName ' Movement During ' exptTitle]);
+    legend(listBuilder,'Location','SouthEast');
+    xlim([0,4]);
 end
 
-legend(subset,'Location','SouthEast');
-workingList = getExperimentsByAnimalAndDate(animalName,subset{1},filterText); % just grab the first expt from list
-workingList = workingList(1,:);
-title(['Movement Across Days: ' workingList{1,2}{1,1}(1:10)]);
-xlim([0,2]);
-
-% for iExpt = 1:size(subset,1)
-%     subplot(1,size(subset,1),iExpt);
-%     title(workingList{iIndex,1}(1:5));
-% end
 
 
