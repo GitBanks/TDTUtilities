16c0707e424a423784619439ed22cbb7f23bdaa6
Sean Grady <smgrady@gmail.com>
2023-04-27 14:25:15 -0500

    Added a new way to bin HTR data, first by animal, then getting animals from a group

---
+++ getPlotHTRBinnedByAnimalDate.m
@@ -0,0 +1,86 @@
+function [allCenters,allCounts] = getPlotHTRBinnedByAnimalDate(animalName,exptDate,binSize,displayFigure)
+
+% new code for plotting HTR!!!!
+% Zarmeen, Mallory, Sean!
+% given an animal name and date
+% 1. create list of indices based on animal name and date
+% 2. load in data
+% 3. return the events and bin centers so we can pool data
+
+% % test params
+% animalName = 'ZZ24';
+% exptDate = '23208';
+
+if ~exist("displayFigure","var")
+    displayFigure = 0;
+end
+
+if ~exist("binSize","var")
+    binSize = 5; % minutes -make this a parameter?
+end
+
+exptList = getExperimentsByAnimalAndDate(animalName,exptDate,'Spon');
+
+% this is a guess that there's only one ctrl!   not advisable!  please change ASAP!!!
+% this is a guess that there's only one ctrl!   not advisable!  please change ASAP!!!
+exptList = exptList(1:2,:); % this is a guess that there's only one ctrl!   not advisable!  please change ASAP!!!
+% this is a guess that there's only one ctrl!   not advisable!  please change ASAP!!!
+% this is a guess that there's only one ctrl!   not advisable!  please change ASAP!!!
+
+if displayFigure
+    figure;
+end
+
+allCenters = [];
+allCounts = [];
+
+for iFile = 1:2
+    fileNameBase = [getPathGlobal('importedData') '20' exptDate(1:2) '\' exptList{iFile} '\' exptList{iFile}];
+    fileNameHTR = [fileNameBase '-HTRevents.mat'];
+    fileNameMagData = [fileNameBase '_magnetData'];
+    load(fileNameMagData)
+    load(fileNameHTR);
+    % == here we need to get the time array, and why not also get movement
+    % all we need is the time from this array
+    downsampleFactor = 10;
+    magData = magData(1:downsampleFactor:end);
+    magDT = magDT*downsampleFactor;
+    timeArrayMag = 0:magDT:(length(magData)-1)*magDT;
+    magData = abs(magData-mean(magData));
+    magData = magData-mean(magData);
+    magData(magData<0) = 0;
+    magData = smooth(magData);
+    % == here we do the HTR work
+    if iFile == 1 % this works, but we need to be really careful about if this is a pre inj 
+        recLength = timeArrayMag(end);
+        timeArrayMag = timeArrayMag - recLength;
+        htrEventTimes = htrEventTimes - recLength;
+    end
+    minuteSummaryEvents = htrEventTimes/60;
+    minuteTimeArray = timeArrayMag/60;
+    edges = round(minuteTimeArray(1):binSize:minuteTimeArray(end));
+    centers = edges+(binSize/2);
+    centers = centers(1:end-1);
+    Y = discretize(minuteSummaryEvents,edges);
+    Y(isnan(Y)) = []; % if they're outside of bounds, they're likely erroneous due to being too early or too late. TODO - maybe handle these differently?
+    [C,~,ic] = unique(Y);
+    a_counts = accumarray(ic,1)';
+    nBins = length(edges)-1;    
+    counts = zeros(1,nBins);
+    counts(C) = a_counts;
+    allCenters = [allCenters centers];
+    allCounts = [allCounts counts];
+end
+
+if displayFigure
+    bar(allCenters,allCounts);
+    xlabel('Time, Mins');
+    ylabel('events');
+    title([animalName ' ' exptDate]); % TODO add a drug?
+    hold on
+    xline(0,'r','LineWidth',3)
+    drawnow;
+end
+
+
+
