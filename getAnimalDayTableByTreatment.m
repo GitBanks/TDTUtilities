16dff94e3a867914981bb48804b41dc3011ddaeb
Sean Grady <smgrady@gmail.com>
2023-05-04 16:50:25 -0500

    added a way to exclude data that should be ignored, also allowed only 1 drug instead of 2

--- getAnimalDayTableByTreatment.m
+++ getAnimalDayTableByTreatment.m
@@ -6,7 +6,10 @@ workingTable = getAnimalsByTreatment(treatment);
 theseDates = unique(workingTable.Date);
 animalDateTable = table();
 tempIndex = 1;
+workingTable(contains(workingTable.Desc,"please ignore"),:) = [];
+
 for iDate = 1:length(theseDates)
+    disp(['working on ' char(theseDates(iDate))]);
     oneDateTable = workingTable(workingTable.Date==theseDates(iDate),:);
     theseDrugs = unique(oneDateTable.Druglist);
 %     if size(theseDrugs) > 1
@@ -24,9 +27,10 @@ for iDate = 1:length(theseDates)
         
         animalDateTable.Drug1(tempIndex) = convertCharsToStrings(treatments.pars{1,1});
         animalDateTable.Drug1Dose(tempIndex) = treatments.vals(1,size(treatments.vals,2));
-        animalDateTable.Drug2(tempIndex) = convertCharsToStrings(treatments.pars{2,1});
-        animalDateTable.Drug2Dose(tempIndex) = treatments.vals(2,size(treatments.vals,2));
-
+        if size(treatments.pars,1) == 2
+            animalDateTable.Drug2(tempIndex) = convertCharsToStrings(treatments.pars{2,1});
+            animalDateTable.Drug2Dose(tempIndex) = treatments.vals(2,size(treatments.vals,2));
+        end
         animalDateTable.Druglist(tempIndex) = theseDrugs(1);
         warning('on','all');
         tempIndex = tempIndex+1;
