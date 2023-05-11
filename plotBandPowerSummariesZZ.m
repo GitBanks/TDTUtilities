56674669e5245da478d36b94d4002563aed257ec
Sean Grady <smgrady@gmail.com>
2023-05-11 11:40:10 -0500

    working version of ZZ box plots with 4 groups. Next steps: rethink data sources and time windows.

--- plotBandPowerSummariesZZ.m
+++ plotBandPowerSummariesZZ.m
@@ -4,7 +4,7 @@
 % list 
 
 
-saveFileName = 'M:\PassiveEphys\mouseEEG\....mat';
+saveFileName = getPathGlobal('ZZ-matTableBandpower');
 load(saveFileName);
 
 
@@ -27,17 +27,17 @@ for iGroup = 1:nGroups
         group(iGroup).movePre(ii,:) = tempT.data(ii,1).pre.move;
         group(iGroup).movePost(ii,:) = tempT.data(ii,1).post.move;
 
-        group(iGroup).avgDeltaPre(ii,:) = tempT.data(ii,1).pre.avgDelta;
-        group(iGroup).avgThetaPre(ii,:) = tempT.data(ii,1).pre.avgTheta;
-        group(iGroup).avgAlphaPre(ii,:) = tempT.data(ii,1).pre.avgAlpha;
-        group(iGroup).avgBetaPre(ii,:) = tempT.data(ii,1).pre.avgBeta;
-        group(iGroup).avgGammaPre(ii,:) = tempT.data(ii,1).pre.avgGamma;
-
-        group(iGroup).avgDeltaPost(ii,:) = tempT.data(ii,1).post.avgDelta;
-        group(iGroup).avgThetaPost(ii,:) = tempT.data(ii,1).post.avgTheta;
-        group(iGroup).avgAlphaPost(ii,:) = tempT.data(ii,1).post.avgAlpha;
-        group(iGroup).avgBetaPost(ii,:) = tempT.data(ii,1).post.avgBeta;
-        group(iGroup).avgGammaPost(ii,:) = tempT.data(ii,1).post.avgGamma;
+%         group(iGroup).avgDeltaPre(ii,:) = tempT.data(ii,1).pre.avgDelta;
+%         group(iGroup).avgThetaPre(ii,:) = tempT.data(ii,1).pre.avgTheta;
+%         group(iGroup).avgAlphaPre(ii,:) = tempT.data(ii,1).pre.avgAlpha;
+%         group(iGroup).avgBetaPre(ii,:) = tempT.data(ii,1).pre.avgBeta;
+%         group(iGroup).avgGammaPre(ii,:) = tempT.data(ii,1).pre.avgGamma;
+% 
+%         group(iGroup).avgDeltaPost(ii,:) = tempT.data(ii,1).post.avgDelta;
+%         group(iGroup).avgThetaPost(ii,:) = tempT.data(ii,1).post.avgTheta;
+%         group(iGroup).avgAlphaPost(ii,:) = tempT.data(ii,1).post.avgAlpha;
+%         group(iGroup).avgBetaPost(ii,:) = tempT.data(ii,1).post.avgBeta;
+%         group(iGroup).avgGammaPost(ii,:) = tempT.data(ii,1).post.avgGamma;
         
         if isfield(tempT.data(groupSize,1).post,'PSMavgDelta')
             group(iGroup).PSMDeltaPre(ii,:) = tempT.data(ii,1).pre.PSMavgDelta;
@@ -53,10 +53,10 @@ end
 
 % % % % boxplot better! % % % %
 % nColsForBoxPlot = 2*3*1+3+6; % number will be: nChans (2) x treatments (4) x just delta (1) + movement (just nTreatment) (4) 
-nColsForBoxPlot = 12; % movement (6) + treatments (6) * front/rear (2) = 18
+nColsForBoxPlot = 12; % movement (4) + treatments (4) * front/rear (2) = 12
 boxplotArray = nan(nColsForBoxPlot,15);
 
-xtickLabelstart = {'Sal','Psilocybin','6FDET','4ACO'}; % changed!
+xtickLabelstart = {'Sal','Psil','4ACO','6FDET'}; % TODO: pull this from the xls file instead, or save that info in the .mat file to pass along to this point..
 colorCodeTreatment = {'k','r','b','g','m','c','k','r','b'};
 indexT = 1;
 
@@ -75,27 +75,12 @@ if isfield(group(end),'PSMDeltaPre') % if we have PSM data all the way through t
     for iGroup = 1:nGroups
         groupSize = length(group(iGroup).movePre);
         boxplotArray(indexT,1:groupSize) = group(iGroup).PSMDeltaPost(:,1)./group(iGroup).PSMDeltaPre(:,1);
-        xtickLabelArray{indexT} = ['Delta ' xtickLabelstart{iGroup} ' A'];
+        xtickLabelArray{indexT} = ['Delta ' xtickLabelstart{iGroup} ' 1'];
         colorCode{indexT} = colorCodeTreatment{iGroup};
         category{indexT} = 'Delta';
         indexT = indexT+1;
         boxplotArray(indexT,1:groupSize) = group(iGroup).PSMDeltaPost(:,2)./group(iGroup).PSMDeltaPre(:,2);
-        xtickLabelArray{indexT} = ['Delta ' xtickLabelstart{iGroup} ' P'];
-        colorCode{indexT} = colorCodeTreatment{iGroup};
-        category{indexT} = 'Delta';
-        indexT = indexT+1;
-    end
-else
-    titleText = 'Delta';
-    for iGroup = 1:nGroups
-        groupSize = length(group(iGroup).movePre);
-        boxplotArray(indexT,1:groupSize) = group(iGroup).avgDeltaPost(:,1)./group(iGroup).avgDeltaPre(:,1);
-        xtickLabelArray{indexT} = ['Delta ' xtickLabelstart{iGroup} ' A'];
-        colorCode{indexT} = colorCodeTreatment{iGroup};
-        category{indexT} = 'Delta';
-        indexT = indexT+1;
-        boxplotArray(indexT,1:groupSize) = group(iGroup).avgDeltaPost(:,2)./group(iGroup).avgDeltaPre(:,2);
-        xtickLabelArray{indexT} = ['Delta ' xtickLabelstart{iGroup} ' P'];
+        xtickLabelArray{indexT} = ['Delta ' xtickLabelstart{iGroup} ' 2'];
         colorCode{indexT} = colorCodeTreatment{iGroup};
         category{indexT} = 'Delta';
         indexT = indexT+1;
@@ -123,10 +108,10 @@ hold on
 
 boxplot(boxplotArray','Colors',char(colorCode));
 xlim([0.5,nColsForBoxPlot+.5]);
-ylabel('Post injection values (t=60:120) divided by baseline values (t=-90:-30)');
+ylabel('Post injection values (t=0:60) divided by baseline values');
 title([titleText ' bandpower changes']);
 yline(1,'--');
-xline(9.5);
+xline(4.5);
 % xline(12.5);
 % xline(15.5);
 % xline(21.5);
@@ -139,10 +124,10 @@ ylim([0,3]);
 legend([a(8) a(6) a(4) a(2)], xtickLabelstart,'Location','northeast');
 
 
-% ax = gca;
-% ax.XTickLabels = {'Sal/Sal move','Sal/LPS move','Flv/LPS move','DMT2.5/LPS move','DMT10/LPS move','BD5/LPS move','BD1/LPS move','BD.1/LPS move','BD/Sal move'...
-%     'Sal/Sal-A','Sal/Sal-P','Sal/LPS-A','Sal/LPS-P','Flv/LPS-A','Flv/LPS-P',...
-%     'DMT2.5/LPS-A','DMT2.5/LPS-P','DMT10/LPS-A','DMT10/LPS-P','BD5/LPS-A','BD5/LPS-P','BD1/LPS-A','BD1/LPS-P','BD.1/LPS-A','BD.1/LPS-P', 'BD/Sal-A','BD/Sal-P'};
+ax = gca;
+ax.XTickLabels = {'Sal move','Psil move','4ACO move','6FDET move',...
+    'Sal-1','Sal-2','Psil-1','Psil-2','4ACO-1','4ACO-2',...
+    '6FDET-1','6FDET-2'};
 
 
 
