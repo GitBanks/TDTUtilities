0d33c80acd56d8fb04a3a412c3e2d7338fe36a79
Sean Grady <smgrady@gmail.com>
2023-07-28 14:44:06 -0500

    added more detailed notes, and a ZZ mouse check

--- fileMaint_passiveEphys.m
+++ fileMaint_passiveEphys.m
@@ -2,6 +2,10 @@ function fileMaint_passiveEphys(animalName,exptDate)
 % test params
 % animalName = 'EEG223';
 % exptDate = '22d18';
+% animalName = 'ZZ29';
+% exptDate = '23427';
+% animalName = 'EEG336';
+% exptDate = '23725';
 
 % 1
 fileMaint(animalName);
@@ -15,34 +19,68 @@ cleanDataByThresholdAnimalDateEEG(animalName,exptDate);
 % This checks for outlier (imported) data points and offers a chance to
 % remove them
 
+% add 60Hz filter here?
+% 1. pick 3 bad days: 
+% animalName = 'EEG308'; exptDate = '23612'    
+% animalName = 'EEG309'; exptDate = '23619'   
+% animalName = 'EEG323'; exptDate = '23706'
+% 2. run a simple notch filter
+% 3. compare
+
+
+
+
+
 % 3 
 patientAnalysis.runAnalysis(@specAnalysis, 'Subjects',{animalName},'Blocks',{exptDate},'isMouse',true,'OptionSet','SegLength4');
 % this is the "ecog" pipeline that creates the spectra data.  Be warned it
 % takes a while - maybe 15-20 mins per animal 
 
 % 4
-chansToExclude = nan;
-setName = 'combined';
-sendToSlack = false;
-plotSpectraEEG(animalName,exptDate,chansToExclude,setName,sendToSlack);
 % This takes the output from #3, and produces three different ways to
 % visualize these data: average spectra by hour, bandpower, and spectrogram
+sendToSlack = false;
+chansToExclude = nan;
+if ~contains(animalName,'ZZ') 
+    setName = 'DOIKetanserin';
+    plotSpectraEEG(animalName,exptDate,chansToExclude,setName,sendToSlack);
+else
+    plotSpectraLFP(animalName,exptDate,chansToExclude,sendToSlack)
+end
 
 % 5
+% WORK IN PROGRESS: saveWindowedArray(animalName,exptDate) will soon save
+% the full windowed (4 sec typically) experiment for the day, and we'll
+% want to rewrite step 5
 PSMTableForR2(animalName,exptDate);
-% creates a table for R to read in, to then run the PSM
+% remember to respect channels Sean
+% creates a table for R to read in, to then run the PSM (fairly simple, just saves this table)
 
 % 6 
-reportPlot = true;
+sendToSlack = true;
 textNotes = 'Seeking evaluation';
-qualityAssuranceEEG(animalName,exptDate,reportPlot,textNotes);
+qualityAssuranceEEG(animalName,exptDate,sendToSlack,textNotes);
 % sends a report to the Slack channel
 
 
 % Next steps:
-% 7. update table here: M:\PassiveEphys\mouseEEG\_______groupInfo.xlsx
-% 8. run some R code: C:\Users\Matt Banks\Documents\Code\TDTUtilities\PSMRunList.R (or ZZ version)
-% 9. run this collectSpectraDataFromExptList(setName)
-% 10. run this plotBandPowerSummaries(setName)
+
+% 7. update table here: M:\PassiveEphys\mouseEEG\_______groupInfo.xlsx  %
+% this is where we track all recordings related to a specific experimental
+% set / group.  This table will be used to combine mouse data.
+
+% 8. run some R code: C:\Users\Matt
+% Banks\Documents\Code\TDTUtilities\PSMRunList.R (or ZZ version) this is
+% the PSM (weighting) code that uses the data from step 5
+
+% 9. run this collectSpectraDataFromExptList(setName) this pulls the data
+% from the list of mice in 7 (according to specified setName) and combines
+% listed PSM (step 8) and movement data into a matlab readable table. "why
+% is this so complicated?" A: because I've been asked for these data 8
+% different ways, 10 different times, and this covers them all.
+
+% 10. run this plotBandPowerSummaries(setName) this plots the table from
+% step 9 - right now it's the pre/post delta PSM(movement weighted) ratio
+% of anterior and posterior for each drug condition.
 
 
