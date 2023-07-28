e96c44b54ba37fc348deb50c84195c7bb54420b2
Sean Grady <smgrady@gmail.com>
2023-07-28 14:43:13 -0500

    sendToSlack seems to have stopped working after maybe an update?  we commented out a section, but this still needs work WIP

--- sendSlackFig.m
+++ sendSlackFig.m
@@ -9,7 +9,7 @@ try
     load([mousePaths.W 'API\slackAPIToken'],'token'); % NOTE: token needs to be the Bot User OAuth Access Token.
     api = SlackAPI(token); 
     channel = '#datachecks'; % specify what channel you would like to upload to
-    SendMsg(api,channel,desc); % this sends the message
+%     SendMsg(api,channel,desc); % this sends the message
     SendFile(api,channel,fpath); % this sends the file
 catch why
     warning(why.message);
